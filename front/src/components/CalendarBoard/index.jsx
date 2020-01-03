a54430166be7407cdd3cc82d4ef95b0244c2dd79
Dragon-taro <dragon.taro.lioil@gmail.com>
2020-01-03 23:49:22 +0900

    2-3

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,52 +1,39 @@
 import React from "react";
 import { GridList } from "@material-ui/core";
+
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+
 import * as styles from "./style.css";
 
-const calendar = [
-  "29",
-  "30",
-  "10月1日",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
-  "13",
-  "14",
-  "15",
-  "16",
-  "17",
-  "18",
-  "19",
-  "20",
-  "21",
-  "22",
-  "23",
-  "24",
-  "25",
-  "26",
-  "27",
-  "28",
-  "29",
-  "30",
-  "31",
-  "11月1日",
-  "2"
-];
+dayjs.locale("ja");
+
+const createCalendar = () => {
+  // 今月の最初の日を追加
+  const firstDay = dayjs().startOf("month");
+
+  // 最初の日の曜日のindexを取得
+  const firstDayIndex = firstDay.day();
+
+  return Array(35)
+    .fill(0)
+    .map((_, i) => {
+      const diffFromFirstDay = i - firstDayIndex;
+      const day = firstDay.add(diffFromFirstDay, "day");
+
+      return day;
+    });
+};
+
+const calendar = createCalendar();
 
 const CalendarBoard = () => {
   return (
     <div className={styles.container}>
       <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
         {calendar.map(c => (
-          <li>
-            <div className={styles.element}>{c}</div>
+          <li key={c.toISOString()}>
+            <div className={styles.element}>{c.format("D")}</div>
           </li>
         ))}
       </GridList>
