ebe9515688b12744b4b5b216867cf6cab515f15c
Dragon-taro <dragon.taro.lioil@gmail.com>
2020-01-03 23:57:29 +0900

    2-5

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GridList } from "@material-ui/core";
+import { GridList, Typography } from "@material-ui/core";
 
 import { createCalendar } from "../../services/calendar";
 
@@ -8,13 +8,28 @@ import * as styles from "./style.css";
 
 const calendar = createCalendar();
 
+const days = ["日", "月", "火", "水", "木", "金", "土"];
+
 const CalendarBoard = () => {
   return (
     <div className={styles.container}>
       <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
+        {days.map(d => (
+          <li key={d}>
+            <Typography
+              className={styles.days}
+              color="textSecondary"
+              align="center"
+              variant="caption"
+              component="div"
+            >
+              {d}
+            </Typography>
+          </li>
+        ))}
         {calendar.map(c => (
           <li key={c.toISOString()}>
-            <CalendarElement>{c.format("D")}</CalendarElement>{" "}
+            <CalendarElement day={c} />
           </li>
         ))}
       </GridList>
--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 
+import { Typography } from "@material-ui/core";
+
 import * as styles from "./style.css";
 
-const CalendarElement = ({ children }) => {
+const CalendarElement = ({ day }) => {
+  // 文字列のフォーマットをどうするか
+  const isFirstDay = day.date() === 1;
+  // 月の最初だけ月情報をつける
+  const format = isFirstDay ? "M月D日" : "D";
+
   return (
     <div className={styles.element}>
-      <div className={styles.date}>{children}</div>
+      <Typography
+        className={styles.date}
+        align="center"
+        variant="caption"
+        component="div"
+      >
+        {day.format(format)}
+      </Typography>
     </div>
   );
 };
