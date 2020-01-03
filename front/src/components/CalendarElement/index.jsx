6f4c0074119393bca21a49b073ec66631328601b
Dragon-taro <dragon.taro.lioil@gmail.com>
2020-01-04 00:00:32 +0900

    2-6

--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -1,24 +1,37 @@
 import React from "react";
-
+import dayjs from "dayjs";
 import { Typography } from "@material-ui/core";
+
+import { isSameMonth, isFirstDay, isSameDay } from "../../services/calendar";
 
 import * as styles from "./style.css";
 
 const CalendarElement = ({ day }) => {
+  const today = dayjs();
+
+  // 今月以外をグレーダウン
+  const isCurrentMonth = isSameMonth(day, today);
+  const textColor = isCurrentMonth ? "textPrimary" : "textSecondary";
+
   // 文字列のフォーマットをどうするか
-  const isFirstDay = day.date() === 1;
   // 月の最初だけ月情報をつける
-  const format = isFirstDay ? "M月D日" : "D";
+  const format = isFirstDay(day) ? "M月D日" : "D";
+
+  // 当日かどうか判断
+  const isToday = isSameDay(day, today);
 
   return (
     <div className={styles.element}>
       <Typography
         className={styles.date}
+        color={textColor}
         align="center"
         variant="caption"
         component="div"
       >
-        {day.format(format)}
+        <span className={isToday ? styles.today : ""}>
+          {day.format(format)}
+        </span>
       </Typography>
     </div>
   );
