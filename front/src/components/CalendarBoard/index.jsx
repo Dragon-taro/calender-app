d8941492a54e60c68f48a2586659a8596495c681
Dragon-taro <dragon.taro.lioil@gmail.com>
2020-01-03 23:44:52 +0900

    2-2

---
+++ front/src/components/CalendarBoard/index.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { GridList } from "@material-ui/core";
+import * as styles from "./style.css";
+
+const calendar = [
+  "29",
+  "30",
+  "10月1日",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24",
+  "25",
+  "26",
+  "27",
+  "28",
+  "29",
+  "30",
+  "31",
+  "11月1日",
+  "2"
+];
+
+const CalendarBoard = () => {
+  return (
+    <div className={styles.container}>
+      <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
+        {calendar.map(c => (
+          <li>
+            <div className={styles.element}>{c}</div>
+          </li>
+        ))}
+      </GridList>
+    </div>
+  );
+};
+
+export default CalendarBoard;
--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <di>hello react!!</di>;
+import CalendarBoard from "./components/CalendarBoard";
+
+const App = () => (
+  <div>
+    <CalendarBoard />
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
