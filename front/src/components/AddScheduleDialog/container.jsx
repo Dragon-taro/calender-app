79fa85a1194e6abc61d2d8d29a914228e7cbe085
Dragon-taro <dragon.taro.lioil@gmail.com>
2020-01-20 00:56:35 +0900

    7-5

--- front/src/components/AddScheduleDialog/container.jsx
+++ front/src/components/AddScheduleDialog/container.jsx
@@ -7,6 +7,7 @@ import {
   addScheduleStartEdit
 } from "../../redux/addSchedule/actions";
 import { asyncSchedulesAddItem } from "../../redux/schedules/effects";
+import { isCloseDialog } from "../../services/schedule";
 
 const mapStateToProps = state => ({ schedule: state.addSchedule });
 
@@ -26,16 +27,27 @@ const mapDispatchToProps = dispatch => ({
   }
 });
 
-const mergeProps = (stateProps, dispatchProps) => ({
-  ...stateProps,
-  ...dispatchProps,
-  saveSchedule: () => {
-    const {
-      schedule: { form: schedule }
-    } = stateProps;
-    dispatchProps.saveSchedule(schedule);
-  }
-});
+const mergeProps = (stateProps, dispatchProps) => {
+  // ==========ここから編集する==========
+  const {
+    schedule: { form: schedule }
+  } = stateProps;
+  const { saveSchedule, closeDialog } = dispatchProps;
+
+  return {
+    ...stateProps,
+    ...dispatchProps,
+    saveSchedule: () => {
+      saveSchedule(schedule);
+    },
+    closeDialog: () => {
+      if (isCloseDialog(schedule)) {
+        closeDialog();
+      }
+    }
+  };
+  // ==========ここまで編集する==========
+};
 
 export default connect(
   mapStateToProps,
--- front/src/services/schedule.js
+++ front/src/services/schedule.js
@@ -12,3 +12,12 @@ export const formatSchedule = schedule => ({
   ...schedule,
   date: dayjs(schedule.date)
 });
+
+export const isCloseDialog = schedule => {
+  const message = "保存されていない変更を破棄しますか？";
+
+  return isScheduleEmpty(schedule) || window.confirm(message);
+};
+
+const isScheduleEmpty = schedule =>
+  !schedule.title && !schedule.description && !schedule.location;
