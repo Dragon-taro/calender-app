d25272794ea1c333b0f697f953a227062a0c6857
yokoto <yo44ko55@gmail.com>
2020-05-01 23:28:06 +0900

    記法ミスを修正

--- redux-sample/src/components/Counter/presentation.jsx
+++ redux-sample/src/components/Counter/presentation.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { increment } from "../../redux/count/actions";
 
-const Counter = ({ count, increment, decrement }) => {
-  console.log(props);
-
-  return (
-    <>
-      <div>counter</div>
-      <button onClick={() => increment(1}}>+</button>
-      <button onClick={() => increment(1}}>+</button>
-    </>
-  );
-};
+const Counter = ({ count, increment, decrement }) => (
+  <>
+    <div>{count}</div>
+    <button onClick={() => increment(1)}>+</button>
+    <button onClick={() => decrement(1)}>+</button>
+  </>
+);
 
 export default Counter;
