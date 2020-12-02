d2fad5b8a32ebb4c201870f179acee75372ee215
akirahashibami <hashibami2207@gmail.com>
2020-12-02 13:18:51 +0900

    toolbar導入

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -13,10 +13,13 @@ dayjs.locale("ja");
 
 import rootReducer from "./redux/rootReducer";
 
+import Navigation from "./components/Navigation/container";
+
 const store = createStore(rootReducer);
 
 const App = () => (
   <Provider store={store} >
+    <Navigation />
     <CalendarBoard />
   </Provider>
 )
