b824b82361802c67f8bf002333559977d82832d0
hyounakura <leopardtender@gmail.com>
2021-01-22 14:40:19 +0900

    decorate calendar

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
-import { GridList } from "@material-ui/core";
+import { GridList, Typography } from "@material-ui/core";
 import * as styles from "./style.css";
 import { createCalendar } from "../../services/calendar";
 import CalendarElement from "../CalendarElement";
 
 const calendar = createCalendar();
+const days = ["日", "月", "火", "水", "木", "金", "土"];
 
 const CalendarBoard = () => {
   return (
     <div className={styles.container}>
       <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
+        {days.map(d => (
+            <li key={d}>
+            <Typography
+              className={styles.days}
+              color="textSecondary"
+              align="center"
+              variant="caption"
+              component="div"
+            >
+              {d}
+            </Typography>
+          </li>
+        ))}
         {calendar.map(c => (
           <li key={c.toISOString()}>
-            <CalendarElement>{c.format("D")}</CalendarElement>
+            <CalendarElement day={c}></CalendarElement>
           </li>
         ))}
       </GridList>
--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -2,10 +2,21 @@ import React from "react";
 
 import * as styles from "./style.css";
 
-const CalendarElement = ({ children }) => {
+import { Typography } from "@material-ui/core";
+
+const CalendarElement = ({ day }) => {
+  const isFirstDay = day.date() === 1;
+  const format = isFirstDay ? "M月D日" : "D";
   return (
     <div className={styles.element}>
-      <div className={styles.date}>{children}</div>
+      <Typography
+        className={styles.date}
+        align="center"
+        variant="caption"
+        component="div"
+      >
+        {day.format(format)}
+      </Typography>
     </div>
   );
 };
