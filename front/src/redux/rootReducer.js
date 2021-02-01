b0ff7b872a1cb1d5383c7f4a42253e31395d8d46
hyounakura <leopardtender@gmail.com>
2021-02-01 22:00:52 +0900

    create schedule action and reducer

--- front/src/redux/rootReducer.js
+++ front/src/redux/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import calendarReducer from "./calendar/reducer";
 import addScheduleReducer from "./addSchedule/reducer";
+import schedulesReducer from "./schedules/reducer";
 
-const rootReducer = combineReducers({ calendar: calendarReducer, addSchedule: addScheduleReducer });
+const rootReducer = combineReducers({ calendar: calendarReducer, addSchedule: addScheduleReducer, schedules: schedulesReducer
+});
 
 export default rootReducer;
