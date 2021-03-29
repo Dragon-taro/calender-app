efca3711d8469f2c8334404c602797d29d5c27ed
吉川徹 <toruyoshikawa@yoshikawatr.lan>
2021-03-29 22:40:47 +0900

    complete 4-3

--- front/src/components/AddScheduleDialog/container.jsx
+++ front/src/components/AddScheduleDialog/container.jsx
@@ -1,12 +1,18 @@
 import AddScheduleDialog from './presentation';
 import { connect } from 'react-redux';
-import { addScheduleCloseDialog } from '../../redux/addSchedule/actions';
+import {
+  addScheduleCloseDialog,
+  addScheduleSetValue
+} from '../../redux/addSchedule/actions';
 
 const mapStateToProps = state => ({ schedule: state.addSchedule });
 
 const mapDispatchToProps = dispatch => ({
   closeDialog: () => {
     dispatch(addScheduleCloseDialog());
+  },
+  setSchedule: (value) => {
+    dispatch(addScheduleSetValue(value));
   }
 });
 
--- front/src/components/AddScheduleDialog/presentation.jsx
+++ front/src/components/AddScheduleDialog/presentation.jsx
@@ -1,12 +1,63 @@
 import React from 'react';
-import { Dialog, DialogContent } from '@material-ui/core';
+import {
+  Dialog,
+  DialogContent,
+  TextField,
+  DialogActions,
+  Button,
+  Input,
+  Grid
+} from '@material-ui/core';
+
+import { LocationOnOutlined, NotesOutlined } from '@material-ui/icons';
+import { withStyles } from '@material-ui/styles';
+
+const spacer = { margin: '4px 0' };
+const Title = withStyles({
+  root: { marginBottom: 32, fontSize: 22 }
+})(Input);
 
 // 引数 = { schedule: { form: { ... }, isDialogOpen: false } }
 // isDialogOpenのみが使える(scheduleは使えない)
-const AddScheduleDialog = ({ schedule: { isDialogOpen }, closeDialog }) => {
+const AddScheduleDialog = ({
+  schedule: {
+    form: { title, location, description },
+    isDialogOpen
+  },
+  closeDialog,
+  setSchedule
+ }) => {
   return (
     <Dialog open={isDialogOpen} onClose={closeDialog} maxWidth='xs' fullWidth>
-      <DialogContent>( :＾∀＾:)</DialogContent>
+      <DialogContent>
+        {/* autoFocus={true} => ダイアログ表示時にfocusが当たっている */}
+        <Title autoFocus fullWidth placeholder="タイトルと日時を追加"
+          value={title} onChange={(e) => setSchedule({ title: e.target.value })} />
+        {/* containerのGridでitemのGridを囲う */}
+        <Grid container spacing={1} alignItems='center' justify="space-between">
+          <Grid item>
+            <LocationOnOutlined />
+          </Grid>
+          <Grid item xs={10}>
+            <TextField style={spacer} fullWidth placeholder="場所を追加"
+              value={location} onChange={(e) => setSchedule({ location: e.target.value })} />
+          </Grid>
+        </Grid>
+        <Grid container spacing={1} alignItems='center' justify='space-between'>
+          <Grid item>
+            <NotesOutlined />
+          </Grid>
+          <Grid item xs={10}>
+            <TextField style={spacer} fullWidth placeholder='説明を追加'
+            value={description} onChange={(e) => setSchedule({ description: e.target.value })} />
+          </Grid>
+        </Grid>
+      </DialogContent>
+      <DialogActions>
+        <Button color='primary' variant='outlined'>
+          保存
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 };
