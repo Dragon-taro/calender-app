21a606b245456e4f4bff9db33351c34a23250106
吉川徹 <toruyoshikawa@yoshikawatr.lan>
2021-04-03 17:54:46 +0900

    complete 5-5

--- front/src/redux/rootReducer.js
+++ front/src/redux/rootReducer.js
@@ -2,13 +2,15 @@ import { combineReducers } from 'redux';
 import calendarReducer from './calendar/reducer';
 import addScheduleReducer from './addSchedule/reducer';
 import schedulesReducer from './schedules/reducer';
+import currentScheduleReducer from './currentSchedule/reducer';
 
 // バラバラのreducerを1つにまとめる
 // { calendar: { year: 2021, month: 3 } } = store.getState()
 const rootReducer = combineReducers({
   calendar: calendarReducer,
   addSchedule: addScheduleReducer,
-  schedules: schedulesReducer
+  schedules: schedulesReducer,
+  currentSchedule: currentScheduleReducer
 });
 
 export default rootReducer;
