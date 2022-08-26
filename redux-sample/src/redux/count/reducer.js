c67f369dbb3985654f2e857db7cef2f91161d2e0
yuta hirano <itrealize@gmail.com>
2022-08-26 23:46:27 +1200

    reduxでインクリメントとデクリメントできるようにアクションと処理を作成しました

---
+++ redux-sample/src/redux/count/reducer.js
@@ -0,0 +1,15 @@
+import { DECREMENT, INCREMENT } from "./constants";
+
+
+const initState = 0;
+
+export const count = (state = initState, {type: payload}) => {
+    switch (type) {
+        case INCREMENT:
+            return state + payload;
+        case DECREMENT:
+            return state - payload;
+        default:
+            return state;
+    }
+};
