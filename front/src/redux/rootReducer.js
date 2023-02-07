113f13cf13e84d134869fbe2ea91be0ca361d080
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2023-02-07 14:46:58 +0900

    予定の詳細を表示する用のReduxを作成

--- front/src/redux/rootReducer.js
+++ front/src/redux/rootReducer.js
@@ -1,11 +1,13 @@
 import { combineReducers} from "redux"
 import  addScheduleReducer  from "./addSchedule/reducer";
 import calendarReducer from "./calendar/reducer";
+import currentScheduleReducer from "./currentSchedule/reducer";
 import schedulesReducer from "./schedules/reducer";
 
 const rootReducer = combineReducers({
   calendar: calendarReducer,
   addSchedule: addScheduleReducer,
+  currentSchedule:currentScheduleReducer,
   schedules: schedulesReducer
 })
 
